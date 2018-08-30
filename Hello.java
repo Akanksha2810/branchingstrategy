932e627ca2d9b6981f647ea40ea9d1240ad59929
John Doe <root@ip-172-31-45-220.us-east-2.compute.internal>
2018-08-30 11:15:45 +0000

    updated bug fixed

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello
-{
+{       
 public static void main(String[] args)
-{
+{                         #testing
 for(int i=1;i<=10;i++)
 System.out.println("Hi");
 System.out.println("check");
